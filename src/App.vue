df4392cf92139f4d762e557d7c34845f35bf1e74
Max.Chou <max.chou@104.com.tw>
2023-01-13 15:40:16 +0800

    6

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite5 + Vue" />
+  <HelloWorld msg="Vite6 + Vue" />
 </template>
 
 <style scoped>
